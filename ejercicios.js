fe9d469f3fb2308395d665cf9591f2008101afd2
Felipemellizo <161835709+Felipemellizo@users.noreply.github.com>
2024-03-01 09:43:14 -0500

    mi primer commit

---
+++ ejercicios.js
@@ -0,0 +1,14 @@
+var nombre = "duvan";
+var apellido= "mellizo";
+var edad = 27 ;
+var numero1=5 ;
+var numero2=3 ;
+var suma = numero1+numero2 ;
+console.log(suma);
+var tengonovia = true ;
+console.log(tengonovia) ;
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengonovia));
+let nombre = duvan
+let
